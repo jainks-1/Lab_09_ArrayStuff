aa3b036bb7d65d08e7556a978920372d0a20f96e
jainks-1 <jainks@mail.uc.edu>
2025-03-14 12:46:33 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,105 @@
-package PACKAGE_NAME;
+ // Kaden Jain 3/14/2025
+
+import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+
+        int[] dataPoints = new int[100];
+
+        // fill each element in the data array with a random number
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1;
+        }
+
+        // print the array
+        for(int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i] + " | ");
+        }
+        System.out.println("\n");
+
+        // loop to calculate sum and average of values
+        int sum = 0;
+        double average = 0.0;
+        for(int i = 0; i < dataPoints.length; i++){
+            sum = sum + dataPoints[i];
+        }
+        average =  (double)sum/dataPoints.length;
+
+        System.out.println("The average of the array is: " + average);
+        System.out.println("The sum of the numbers is: " + sum);
+        System.out.println();
+
+        // prompt and input an int value between 1 and 100 from the user
+        int userNum = 0;
+        userNum = SafeInput.getRangedInt(in, "Please enter an integer: ", 1, 100);
+
+        // find all userNum instances in array
+        int count = 0;
+        for (int num : dataPoints) {
+            if (num == userNum) {
+                count++;
+            }
+        }
+        // Show result
+        if (count > 0) {
+            System.out.println("The number " + userNum + " was found " + count + " times in the array.");
+        } else {
+            System.out.println("The number " + userNum + " was not found in the array.");
+        }
+
+        // prompt and input an int value between 1 and 100 from the user
+        int secondUserNum = 0;
+        secondUserNum = SafeInput.getRangedInt(in, "\nPlease enter another integer: ", 1, 100);
+
+        // find the first occurrence of secondUserNum in array
+        int position = -1; // Default to not found
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == secondUserNum) {
+                position = i; // Store the index where  value is found
+                break; // Exit loop when value found
+            }
+        }
+        // Show result
+        if (position != -1) {
+            System.out.println("The value " + secondUserNum + " was found at array index " + position + ".");
+        } else {
+            System.out.println("The value " + secondUserNum + " was not found in the array.");
+        }
+
+        // Initialize min and max with using first element
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+
+        // Loop to find min and max
+        for (int i = 1; i < dataPoints.length; i++) {
+            if (dataPoints[i] < min) {
+                min = dataPoints[i]; // Updates min if a smaller value is found
+            }
+            if (dataPoints[i] > max) {
+                max = dataPoints[i]; // Updates max if a larger value is found
+            }
+        }
+        // Show results
+        System.out.println("\nThe minimum value in the array is: " + min);
+        System.out.println("The maximum value in the array is: " + max);
+
+        System.out.println("\nAverage of the dataPoints array is: " + getAverage(dataPoints));
+
+    }
+
+    public static double getAverage(int values[])
+    {
+        int sum = 0;
+        for(int i = 0; i < values.length; i++){
+            sum = sum + values[i];
+            // System.out.println(sum); shows each iteration of the sum for debugging
+        }
+        return (double)sum/values.length; // use (double) because dividing int by an int
+    }
+
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,15 +1,220 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class SafeInput {
+
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        do
+        {
+            System.out.print("\n" +prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+
     }
+
+
+
+    public static int getInt(Scanner pipe, String prompt) {
+        int value = 0;
+        boolean done = false;
+        String trash;
+
+        do {
+            System.out.print(prompt + ": ");
+
+            if (pipe.hasNextInt()) {
+                value = pipe.nextInt();
+                done = true;
+            } else {
+                trash = pipe.next();
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+            pipe.nextLine(); // Clear the buffer
+        } while (!done);
+
+        return value;
+    }
+
+
+
+
+    public static double getDouble(Scanner pipe, String prompt) {
+        double value = 0;
+        boolean done = false;
+        String trash;
+
+        do {
+            System.out.print(prompt + ": ");
+
+            if (pipe.hasNextDouble()) {
+                value = pipe.nextDouble();
+                done = true;
+            } else {
+                trash = pipe.next();
+                System.out.println("Invalid input. Please enter a valid double.");
+            }
+            pipe.nextLine(); // Clear the buffer
+        } while (!done);
+
+        return value;
+    }
+
+
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+        int result = 0;
+        boolean done = false;
+        String trash = "";
+
+        do{
+            System.out.println(prompt+"[" + low + "-" + high + "]: ");
+            if(pipe.hasNextInt()) {
+                result = pipe.nextInt();;
+                pipe.nextLine();
+
+                if(result >= low && result <+ high) {
+                    done = true;
+                }
+                else {
+                    System.out.println("You must enter a value in the range [" + low + "-" + high + "]");
+                }
+            }
+            else{
+                trash = pipe.nextLine();
+                System.out.println("You must enter a value in the range [" + low + "-" + high + "]");
+            }
+        }
+        while (!done);
+
+        return result;
+    }
+
+
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high) {
+        double result = 0;
+        boolean done = false;
+        String trash = "";
+
+        do{
+            System.out.println(prompt+"[" + low + "-" + high + "]: ");
+            if(pipe.hasNextDouble()) {
+                result = pipe.nextDouble();;
+                pipe.nextLine();
+
+                if(result >= low && result <+ high) {
+                    done = true;
+                }
+                else {
+                    System.out.println("You must enter a value in the range [" + low + "-" + high + "]");
+                }
+            }
+            else{
+                trash = pipe.nextLine();
+                System.out.println("You must enter a value in the range [" + low + "-" + high + "]");
+            }
+        }
+        while (!done);
+
+        return result;
+    }
+
+
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt) {
+        String response;
+        boolean done = false;
+        boolean result = false;
+
+        do {
+            System.out.print(prompt + " [Y/N]: ");
+            response = pipe.nextLine();
+
+            if (response.equalsIgnoreCase("Y")) {
+                result = true;
+                done = true;
+            } else if (response.equalsIgnoreCase("N")) {
+                result = false;
+                done = true;
+            } else {
+                System.out.println("Invalid input. Please enter 'Y' for Yes or 'N' for No.");
+            }
+        } while (!done);
+
+        return result;
+    }
+
+
+
+
+    public static String getRegExString(Scanner pipe, String prompt, String regExPattern) {
+        String value = "";
+        boolean done = false;
+
+        do{
+            System.out.println(prompt + ":");
+            value = pipe.nextLine();
+
+            if(value.matches(regExPattern)){
+                done=true;
+            }
+            else{
+                System.out.print("\n Invalid input: " + value);
+            }
+
+        }while(!done);
+
+        return value;
+    }
+
+
+
+    public static void prettyHeader(String msg) {
+        // Define the width for header
+        int width = 60;
+
+        // Calculate length of message
+        int msgLength = msg.length();
+
+        // Calculate padding before and after message to center - also 3 stars at beginning and end
+        int padding = (width - 6 - msgLength) / 2;
+
+        // Create top and bottom star rows
+        for (int i = 0; i < width; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        // Print middle row with message
+        System.out.print("***");
+        for (int i = 0; i < padding; i++) {
+            System.out.print(" ");
+        }
+        System.out.print(msg);
+        for (int i = 0; i < padding; i++) {
+            System.out.print(" ");
+        }
+        // for when padding is odd to center
+        if ((width - 6 - msgLength) % 2 != 0) {
+            System.out.print(" ");
+        }
+        System.out.println("***");
+
+        // Print bottom row of stars
+        for (int i = 0; i < width; i++) {
+            System.out.print("*");
+        }
+        System.out.println();  // Move to the next line
+    }
+
+
+
+    /* -- SOME COMMENTS TO NOTE
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
 }
